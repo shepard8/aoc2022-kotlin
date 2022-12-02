1a606949d85ef895713a9d717d919fe2dae5a0b9
Fabian Pijcke <fabian.pijcke@gmail.com>
2022-12-02 20:12:42 +0100

    More meaningful names.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,14 @@
 fun main() {
-    fun part1(input: List<Int>): Int {
-        return input.max()
-    }
+    fun part1(sums: List<Int>) = sums.max()
 
-    fun part2(input: List<Int>): Int {
-        return input.sortedDescending().take(3).sum()
-    }
+    fun part2(sums: List<Int>) = sums.sortedDescending().take(3).sum()
 
     val input = readInput("Day01")
     val sums = sequence {
         var curSum = 0
 
         input.forEach { line ->
-            if (line.isNullOrBlank()) {
+            if (line.isBlank()) {
                 yield(curSum)
                 curSum = 0
             }
@@ -23,6 +19,7 @@ fun main() {
 
         yield(curSum)
     }.toList()
+
     println(part1(sums))
     println(part2(sums))
 }
